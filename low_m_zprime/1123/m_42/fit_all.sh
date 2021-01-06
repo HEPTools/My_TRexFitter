cdd0f76b48ed201cb899f07e96c7ab120e6e2b31
Zhe Yang <starprecursor@gmail.com>
2021-01-06 20:05:53 +0100

    Add example config

---
+++ low_m_zprime/1123/m_42/fit_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if
+    [ -z "$1"]
+then
+    find . -name "zprime*.config" -print0 | while read -d $'\0' file; do
+        trex-fitter n ./$file
+        trex-fitter d ./$file
+        trex-fitter w ./$file
+        trex-fitter f ./$file
+        trex-fitter p ./$file
+        trex-fitter l ./$file
+    done
+else
+    trex-fitter n ./$1
+    trex-fitter d ./$1
+    trex-fitter w ./$1
+    trex-fitter f ./$1
+    trex-fitter p ./$1
+    trex-fitter l ./$1
+fi
